c9bbd62b749f9434d6f7b184d83a8508848a7a7f
SagarUlli <sagarulli5@gmail.com>
2026-04-24 23:57:59 +0530

    fix: user follow mapping (ManyToMany + join table)

--- src/main/java/org/jsp/jsp_gram/dto/User.java
+++ src/main/java/org/jsp/jsp_gram/dto/User.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Transient;
 import lombok.Data;
@@ -33,10 +34,11 @@ public class User {
 	private String imageUrl;
 	private boolean prime;
 
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany
+	@JoinTable(name = "user_following", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "following_id"))
 	private List<User> following = new ArrayList<>();
 
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany(mappedBy = "following")
 	private List<User> followers = new ArrayList<>();
 
 	/**
